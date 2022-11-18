fa4b8b30731c31deefb25662f7cdd44a3367da5f
Cl0wnFiesta <clownfiesta98@gmail.com>
2022-11-18 14:43:47 +0200

    all done

--- src/App.js
+++ src/App.js
@@ -1,64 +1,72 @@
-import { useState } from 'react';
+import { useState } from "react";
+import AddTodo from "./AddTodo.js";
+import TaskList from "./TaskList.js";
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
 
-const initialProducts = [{
-  id: 0,
-  name: 'Baklava',
-  count: 1,
-}, {
-  id: 1,
-  name: 'Cheese',
-  count: 5,
-}, {
-  id: 2,
-  name: 'Spaghetti',
-  count: 2,
-}];
-
-export default function ShoppingCart() {
-  const [
-    products,
-    setProducts
-  ] = useState(initialProducts)
+function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
 
   function handleIncreaseClick(productId) {
-    setProducts(products.map(product => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count + 1
-        };
-      } else {
-        return product;
-      }
-    }))
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count + 1,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
   }
-function handleDecreaseClick(productId) {
-    setProducts(products.map(product => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count>0 ? product.count -1 : product.count 
-        };
-      } else {
-        return product;
-      }
-    }))
+  function handleDecreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count > 0 ? product.count - 1 : product.count,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
   }
   return (
     <ul>
-      {products.map(product => (
+      {products.map((product) => (
         <li key={product.id}>
-          {product.name}
-          {' '}
-          (<b>{product.count}</b>)
-          <button onClick={() => {
-            handleIncreaseClick(product.id);
-          }}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
             +
           </button>
-          <button onClick={() => {
-            handleDecreaseClick(product.id);
-          }}>
+          <button
+            onClick={() => {
+              handleDecreaseClick(product.id);
+            }}
+          >
             -
           </button>
         </li>
@@ -66,3 +74,66 @@ function handleDecreaseClick(productId) {
     </ul>
   );
 }
+
+
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: "Buy milk", done: true },
+  { id: 1, title: "Eat tacos", done: false },
+  { id: 2, title: "Brew tea", done: false },
+];
+
+function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false,
+      },
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(
+      todos.map((x) => {
+        if (x.id === nextTodo.id) {
+          // Create a *new* object with changes
+          return nextTodo;
+        } else {
+          // No changes
+          return x;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter((a) => a.id !== todoId));
+  }
+
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
+}
+
+const App = () => {
+  return (
+  <>
+  <ShoppingCart></ShoppingCart>
+  <TaskApp></TaskApp>
+  </>
+  );
+};
+export default App;
